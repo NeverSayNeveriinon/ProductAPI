5d94b0ee4ecae6eeeaf22eedd26f9a4cf576da74
Ali Hemmati <alihemmati1081@gmail.com>
2024-05-29 01:59:48 +0330

    Create 'Product' entity

--- Core/Domain/Product.cs
+++ Core/Domain/Product.cs
@@ -4,5 +4,16 @@ namespace Core.Domain;
 
 public class Product
 {
-
+    [StringLength(50)]
+    public string Name { get; set; }
+    
+    public DateTime ProduceDate { get; set; }
+    
+    [EmailAddress]
+    public string? ManufactureEmail { get; set; }
+    
+    [Phone]
+    public string? ManufacturePhone { get; set; }
+    
+    public bool IsAvailable { get; set; }
 }
