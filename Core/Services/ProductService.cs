588d84731d30f5cdb6011a455c36bd525229182d
Ali Hemmati <alihemmati1081@gmail.com>
2024-05-30 04:14:58 +0330

    The Initial Setup for Testing 'ProductService'

---
+++ Core/Services/ProductService.cs
@@ -0,0 +1,41 @@
+﻿using Core.Domain;
+using Core.Domain.RepositoryContracts;
+using Core.DTO;
+using Core.ServiceContracts;
+
+namespace Core.Services;
+
+public class ProductService : IProductService
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductService(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public Task<ProductResponse> AddProduct(ProductRequest? productAddRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ProductResponse>> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductResponse?> GetProductByID(Guid? ID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductResponse?> UpdateProduct(ProductRequest? productUpdateRequest, Guid? productID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool?> DeleteProduct(Guid? ID)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ XUnit_Tests/UnitTest/Services/ProductServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+
+using Core.Domain.RepositoryContracts;
+using Core.ServiceContracts;
+using Core.Services;
+
+namespace XUnit_Tests.UnitTest.Services;
+
+public class ProductServiceTest
+{
+    private readonly IProductService _productService;
+        
+    private readonly IProductRepository _productRepository;
+    private readonly Mock<IProductRepository> _productRepositoryMock;
+
+    public ProductServiceTest()
+    {
+        _productRepositoryMock = new Mock<IProductRepository>();
+        _productRepository = _productRepositoryMock.Object;
+
+        _productService = new ProductService(_productRepository);
+    }
+
+
+}
