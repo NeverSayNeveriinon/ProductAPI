91853729e628b8d925e530511d87445d09eb3e29
Ali Hemmati <alihemmati1081@gmail.com>
2024-05-31 21:19:37 +0330

    Delete Some Properties of 'ProductRequest'

--- Core/Domain/ApplicationUser.cs
+++ Core/Domain/ApplicationUser.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace Core.Domain;
 
 public class ApplicationUser : IdentityUser<Guid>
 {
+    [StringLength(60)]
+    public override string Email { get; set; }
+    
+    [StringLength(maximumLength:11, MinimumLength = 11)]
+    public override string? PhoneNumber { get; set; }
 }
