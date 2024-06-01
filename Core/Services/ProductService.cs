131ae2ca81c962a1921bcdd9d4618017b0123bc5
Ali Hemmati <alihemmati1081@gmail.com>
2024-06-01 14:23:11 +0330

    Add 'AddProduct' Method in 'ProductService'

--- Core/Services/ProductService.cs
+++ Core/Services/ProductService.cs
@@ -3,6 +3,8 @@ using Core.Domain.RepositoryContracts;
 using Core.DTO;
 using Core.Helpers;
 using Core.ServiceContracts;
+using Mapster;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 
 namespace Core.Services;
@@ -10,18 +12,55 @@ namespace Core.Services;
 public class ProductService : IProductService
 {
     private readonly IProductRepository _productRepository;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    // private readonly HttpContext _httpContext;
     private readonly UserManager<ApplicationUser> _userManager;
 
-    public ProductService(IProductRepository productRepository, UserManager<ApplicationUser> userManager)
+    public ProductService(IProductRepository productRepository, UserManager<ApplicationUser> userManager, IHttpContextAccessor httpContextAccessor)
     {
         _productRepository = productRepository;
         _userManager = userManager;
+        _httpContextAccessor = httpContextAccessor;
+        // _httpContext = httpContextAccessor.HttpContext!;
     }
 
 
-    public Task<ProductResponse> AddProduct(ProductRequest? productAddRequest)
+    public async Task<ProductResponse> AddProduct(ProductRequest? productAddRequest)
     {
-        throw new NotImplementedException();
+        // 'productAddRequest' is Null //
+        if (productAddRequest == null)
+        {
+            throw new ArgumentNullException("'ProductAddRequest' object is Null");
+        }
+
+        // 'productAddRequest.Name' is Null //
+        if (string.IsNullOrEmpty(productAddRequest.Name))
+        {
+            throw new ArgumentNullException("The 'Product Name' in 'ProductAddRequest' object can't be blank");
+        }
+        
+        // Other Validations
+        ValidationHelper.ModelValidation(productAddRequest);
+
+        // 'productAddRequest.Name' is Duplicate //
+        if ( (await _productRepository.GetFilteredProducts(product => product.Name == productAddRequest.Name))?.Count > 0)
+        {
+            throw new ArgumentException("The 'Product Name' is already exists");
+        }
+
+        
+        // Converting from 'ProductAddRequest' to 'Product'
+        Product product = productAddRequest.Adapt<Product>();
+        product.ProduceDate = DateTime.Now;
+        product.ManufactureEmail = _httpContextAccessor.HttpContext!.User.Identity!.Name!;
+        product.ManufacturePhone =  _userManager.GetUserAsync(_httpContextAccessor.HttpContext.User).Result!.PhoneNumber;
+        
+        // adding the object to db
+        await _productRepository.AddProduct(product);
+
+        // Converting from 'Product' to 'ProductResponse'
+        ProductResponse productResponse = product.Adapt<ProductResponse>();
+        return productResponse;
     }
 
     public Task<List<ProductResponse>> GetAllProducts()
