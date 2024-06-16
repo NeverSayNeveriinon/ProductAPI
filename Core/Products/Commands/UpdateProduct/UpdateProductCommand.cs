b96ba329c2848f69210d9be6536aa3621034f7e9
Ali Hemmati <alihemmati1081@gmail.com>
2024-06-16 15:40:21 +0330

    Migrate 'UpdateProduct' from Service Layer to 'Handler' method in Commands layer

---
+++ Core/Products/Commands/UpdateProduct/UpdateProductCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MediatR;
+
+
+namespace Core.DTO.Product;
+
+public class UpdateProductCommand : IRequest<ProductResponse?>
+{
+    public ProductKey? ProductKey;
+    
+    [Required(ErrorMessage = "The 'Product Name' Can't Be Blank!!!")]
+    [StringLength(50, ErrorMessage = "The 'Product Name' Can't Be More Than 50 Characters")]
+    public string Name { get; set; }
+    
+    [Required(ErrorMessage = "The 'Availability of Product' should be specified!!!")]
+    public bool? IsAvailable { get; set; }
+}
---
+++ Core/Products/Commands/UpdateProduct/UpdateProductCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using Core.Domain;
+using Core.Domain.RepositoryContracts;
+using Core.DTO.Product;
+using Mapster;
+using MediatR;
+
+namespace Core.Products.Commands.UpdateProduct;
+
+public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, ProductResponse?>
+{
+    private readonly IProductRepository _productRepository;
+
+    public UpdateProductCommandHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+    
+    
+    public async Task<ProductResponse?> Handle(UpdateProductCommand? request, CancellationToken cancellationToken)
+    {
+        // if 'product ID' is null
+        ArgumentNullException.ThrowIfNull(request?.ProductKey,"The 'ProductKey' parameter is Null");
+        
+        // if 'request' is null
+        ArgumentNullException.ThrowIfNull(request,"The 'ProductRequest' object parameter is Null");
+
+        // 'request.Name' is Empty or Null//
+        ArgumentException.ThrowIfNullOrEmpty(request.Name,"The 'Product Name' in 'ProductRequest' object can't be blank");
+
+        
+        Product? product = await _productRepository.GetProductByKey(request.ProductKey);
+        
+        // if 'key' is invalid (doesn't exist)
+        if (product == null)
+        {
+            return null;
+        }
+            
+        Product updatedProduct = await _productRepository.UpdateProduct(product, request.Adapt<Product>());
+
+        return updatedProduct.Adapt<ProductResponse>();
+    }
+}
