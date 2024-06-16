0706796a146b003c7f2368f000133a3e973907a5
Ali Hemmati <alihemmati1081@gmail.com>
2024-06-16 15:40:44 +0330

    Migrate 'GetAllProducts' from Service Layer to 'Handler' method in Query layer

---
+++ Core/Products/Queries/GetAllProducts/GetAllProductsQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MediatR;
+
+
+namespace Core.DTO.Product;
+
+public class GetAllProductsQuery : IRequest<List<ProductResponse>>
+{
+    [EmailAddress(ErrorMessage = "The 'Email Address' Should Be in a Proper Format!!!")]
+    public string? FilterByEmail;
+}
---
+++ Core/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Core.Domain;
+using Core.Domain.RepositoryContracts;
+using Core.DTO.Product;
+using Core.Helpers;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+
+namespace Core.Products.Commands.CreateProduct;
+
+public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery,List<ProductResponse>>
+{
+    private readonly IProductRepository _productRepository;
+    
+    public GetAllProductsQueryHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+    
+    
+    public async Task<List<ProductResponse>> Handle(GetAllProductsQuery query, CancellationToken cancellationToken)
+    {
+        List<Product> products = new List<Product>();
+        if (!string.IsNullOrEmpty(query.FilterByEmail))
+        {
+            products = await _productRepository.GetFilteredProducts(record => record.ManufactureEmail == query.FilterByEmail);
+        }
+        else
+        {
+            products = await _productRepository.GetAllProducts();
+        }
+
+        List<ProductResponse> productsResponses = products.Select(prouduct => prouduct.Adapt<ProductResponse>()).ToList();
+        return productsResponses;
+    }
+}
