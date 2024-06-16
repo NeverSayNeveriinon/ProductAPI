2facf2729a743c85f889c8d192cee10cd26bfad9
Ali Hemmati <alihemmati1081@gmail.com>
2024-06-16 15:40:10 +0330

    Migrate 'DeleteProduct' from Service Layer to 'Handler' method in Commands layer

---
+++ Core/Products/Commands/DeleteProduct/DeleteProductCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Core.DTO.Product;
+using MediatR;
+
+namespace Core.Products.Commands.DeleteProduct;
+
+public class DeleteProductCommand : IRequest<bool?>
+{
+    [Required(ErrorMessage = "The 'Product Key' Can't Be Blank!!!")]
+    public ProductKey? ProductKey;
+}
---
+++ Core/Products/Commands/DeleteProduct/DeleteProductCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Core.Domain;
+using Core.Domain.RepositoryContracts;
+using Core.DTO.Product;
+using MediatR;
+
+namespace Core.Products.Commands.DeleteProduct;
+
+public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand, bool?>
+{
+    private readonly IProductRepository _productRepository;
+    
+    public DeleteProductCommandHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+    
+    
+    public async Task<bool?> Handle(DeleteProductCommand? request, CancellationToken cancellationToken)
+    {
+        // if 'Key' is null
+        ArgumentNullException.ThrowIfNull(request?.ProductKey,"The 'Product Key' parameter is Null");
+
+        Product? product = await _productRepository.GetProductByKey(request.ProductKey);
+        
+        // if 'Key' is invalid (doesn't exist)
+        if (product == null) 
+        {
+            return null;
+        }
+    
+        bool result = await _productRepository.DeleteProduct(product);
+            
+        return result;
+    }
+}
